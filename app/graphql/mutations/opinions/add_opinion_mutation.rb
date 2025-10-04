772510596d51c8a190c583d677cc5c1b7c568975
Michał Siwiec <michalsiwiec@MacBook-Air-Micha.local>
2025-10-04 12:36:58 +0200

    refactor

--- app/graphql/mutations/opinions/add_opinion_mutation.rb
+++ app/graphql/mutations/opinions/add_opinion_mutation.rb
@@ -8,7 +8,10 @@ module Mutations
 
       def resolve(params)
         super(params)
-        ::Opinions::AddOpinionService.call(params: @params)
+        user = ::Session::UserSessionService.new(session: context.fetch(:session)).current_user
+        raise ActiveRecord::RecordNotFound, 'User not found' unless user
+
+        Opinion.create!(content: @params.fetch(:content), mark: @params.fetch(:mark), user_id: user.id)
       end
     end
   end
